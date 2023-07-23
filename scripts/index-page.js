a63d0616c94dad7763a823685b0fb4cd9f61e255
Lester Blake <lstr.mail@gmail.com>
2023-07-23 16:47:55 -0400

    completed js code for axios.get, .post

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,99 +1,69 @@
-axios.get('https://project-1-api.herokuapp.com/comments?api_key=14b3921c-334f-4538-9077-206ad4e84fea')
+let commentContainer = document.querySelector('.comment');
+//create  function here (load comments)
+
+function loadComments () {
+  axios.get('https://project-1-api.herokuapp.com/comments?api_key=14b3921c-334f-4538-9077-206ad4e84fea')
   .then(response => {
-    console.log(response.data)
+    const responseData = response.data
+    console.log(responseData)
+    responseData.forEach(comment => {
+      let commentCard = document.createElement("div");
+      commentCard.classList.add('comment__card');
+  
+      let commentDivLeft = document.createElement("div");
+      commentDivLeft.classList.add('comment__div--left');
+  
+      let commentImg = document.createElement("img");
+      commentImg.classList.add('comment__card-img');
+      commentImg.src = "./assets/images/comment_placeholder_img.png";
+  
+      let commentDivRight = document.createElement("div");
+      commentDivRight.classList.add('comment__div--right');
+  
+      let commentDivMid = document.createElement("div");
+      commentDivMid.classList.add('comment__div--middle');
+  
+      let commentCardName = document.createElement("h3");
+      commentCardName.classList.add('comment__card-name');
+      commentCardName.innerHTML = comment.name;
+  
+      let commentCardDate = document.createElement("h4");
+      commentCardDate.classList.add('comment__card-date');
+      commentCardDate.innerHTML = comment.timestamp;
+  
+      let commentDivBtm = document.createElement("div");
+      commentDivBtm.classList.add('comment__div--bottom');
+  
+      let commentDvd = document.createElement("hr");
+      commentDvd.classList.add('divider');
+  
+      let commentText = document.createElement("p");
+      commentText.classList.add('comment__text');
+      commentText.innerHTML = comment.comment;
+  
+      commentCard.appendChild(commentDivLeft);
+      commentCard.appendChild(commentDivRight);
+  
+      commentDivLeft.appendChild(commentImg);
+  
+      commentDivRight.appendChild(commentDivMid);
+      commentDivRight.appendChild(commentDivBtm);
+  
+      commentDivMid.appendChild(commentCardName);
+      commentDivMid.appendChild(commentCardDate);
+  
+      commentDivBtm.appendChild(commentText);
+  
+      commentContainer.appendChild(commentDvd);
+      commentContainer.appendChild(commentCard);
+    });
   })
   .catch(function (error) {
     console.log(error)
   });
+}
+loadComments()
 
-
-
-
-
-//Comments Array to be Display on page load
-commentsArr = [
-  {
-    name: "Connor Walton",
-    date: "02/17/2022",
-    comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    name: "Emile Beach",
-    date: "01/19/2022",
-    comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-  },
-  {
-    name: "Miles Acosta",
-    date: "12/20/2022",
-    comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-  }
-];
-
-const addCommentToArray = (name, date, comment) => {
-  commentsArr.push({
-    name: name,
-    date: date,
-    comment: comment
-  });
-};
-
-const displayComments = () => {
-  const commentContainer = document.querySelector(".comment");
-
-  commentsArr.forEach(comment => {
-    let commentCard = document.createElement("div");
-    commentCard.classList.add('comment__card');
-
-    let commentDivLeft = document.createElement("div");
-    commentDivLeft.classList.add('comment__div--left');
-
-    let commentImg = document.createElement("img");
-    commentImg.classList.add('comment__card-img');
-    commentImg.src = "./assets/images/comment_placeholder_img.png";
-
-    let commentDivRight = document.createElement("div");
-    commentDivRight.classList.add('comment__div--right');
-
-    let commentDivMid = document.createElement("div");
-    commentDivMid.classList.add('comment__div--middle');
-
-    let commentCardName = document.createElement("h3");
-    commentCardName.classList.add('comment__card-name');
-    commentCardName.innerHTML = comment.name;
-
-    let commentCardDate = document.createElement("h4");
-    commentCardDate.classList.add('comment__card-date');
-    commentCardDate.innerHTML = comment.date;
-
-    let commentDivBtm = document.createElement("div");
-    commentDivBtm.classList.add('comment__div--bottom');
-
-    let commentDvd = document.createElement("hr");
-    commentDvd.classList.add('divider');
-
-    let commentText = document.createElement("p");
-    commentText.classList.add('comment__text');
-    commentText.innerHTML = comment.comment;
-
-    commentCard.appendChild(commentDivLeft);
-    commentCard.appendChild(commentDivRight);
-
-    commentDivLeft.appendChild(commentImg);
-
-    commentDivRight.appendChild(commentDivMid);
-    commentDivRight.appendChild(commentDivBtm);
-
-    commentDivMid.appendChild(commentCardName);
-    commentDivMid.appendChild(commentCardDate);
-
-    commentDivBtm.appendChild(commentText);
-
-    commentContainer.appendChild(commentDvd);
-    commentContainer.appendChild(commentCard);
-  });
-};
-//function to display comments on page load
-displayComments();
 
 //HANDLE FORM SUBMISSION
 const formSubmission = () => {
@@ -107,11 +77,6 @@ const formSubmission = () => {
 
   let currentDate = getCurrentDate();
 
-  //ERROR SCRIPTING
-  /**
-   * The issue here is that it is applying the error styling regardless of rules
-   * It also continues to post to the array despite being an error
-   */
   const formError = document.querySelectorAll('.data-comment');
 
   if (!userCommentName || !userCommentText) {
@@ -121,7 +86,13 @@ const formSubmission = () => {
     });
   } else {
     console.log("submitted");
-    addCommentToArray(userCommentName, currentDate, userCommentText);
+    // Prepare the comment data
+    const commentData = {
+      commenterName: userCommentName,
+      userComment: userCommentText
+    };
+    // Pass the comment data to postComment
+    postComment(commentData);
   }
 
   //Creates visual components of the comment DISPLAY
@@ -179,17 +150,20 @@ const formSubmission = () => {
   //append to divBottom
   commentDivBtm.appendChild(commentText);
 
-  //append divider to bottom of comment card
-  // commentCard.appendChild(commentDvd);
-
   //append to comment parent
   document.querySelector(".comment").prepend(commentCard);
   document.querySelector(".comment").prepend(commentDvd);
 
   clearAllInputs();
+  //To fix scope issues for postComments() these are returned as an object
+  return {
+    userCommentText: userCommentText,
+    userCommentName: userCommentName
+  };
 }
 
-//CODE HOLDING AREA 
+
+//Functions HOLDING AREA 
 function getCurrentDate() {
   const currentDate = new Date();
   let month = currentDate.getMonth() + 1;
@@ -206,7 +180,33 @@ function clearAllInputs(event) {
 const commentButton = document.querySelector(".info__button");
 commentButton.addEventListener("click", function (e) {
   e.preventDefault();
-  formSubmission();
+  const commentData = formSubmission();
+  postComment(commentData);
 });
+
+function fetchComments () {
+  axios.get('https://project-1-api.herokuapp.com/comments?api_key=14b3921c-334f-4538-9077-206ad4e84fea')
+    .then(response => {
+    const responseData = response.data
+    console.log(responseData)
+    })
+    .catch(function (error) {
+    console.log(error)
+  });
+}
+
+function postComment(commentData) {
+  axios.post('https://project-1-api.herokuapp.com/comments?api_key=14b3921c-334f-4538-9077-206ad4e84fea', {
+    commenterName: commentData.userCommentText,
+    userComment: commentData.userCommentName,
+  })
+  .then(function (response) {
+    fetchComments();
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
 
 
